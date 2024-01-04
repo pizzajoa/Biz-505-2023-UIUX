6b45c20de11cdce3b7e1916a0e094e2f43a7dd0f
pizzajoa <jjh0954@naver.com>
2024-01-04 15:55:40 +0900

    오후

---
+++ UI_005/nav.js
@@ -0,0 +1,3 @@
+const nav-home =document.querySelector("li.nav_main.home");
+const nav-login =document.querySelector("li.nav_main.home");
+const nav-join =document.querySelector("li.nav_main.home");
