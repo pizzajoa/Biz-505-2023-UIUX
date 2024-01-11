2ef46dc8bb991ed9346b7dcb7fddb92d21500a1f
pizzajoa <jjh0954@naver.com>
2024-01-11 11:50:27 +0900

    오전

--- UI_005/js/image1.js
+++ UI_005/js/image1.js
@@ -1,21 +1,19 @@
-const img_TAG_NAME = "IMG";
+const IMG_TAG_NAME = "IMG";
 const ALT_TEXT = {
   인증사진: "인증사진",
   바라보기: "바라보기",
   스피츠: "스피츠",
   눈치보기: "눈치보기",
-  놀아죠: "놀아죠",
+  놀아죠: "놀아조",
   첫만남: "첫_만남",
 };
 const img_container = document.querySelector("section.img_container");
 const img_container_click_handler = (event) => {
   const target = event.target;
   const tag_name = target.tagName;
-
-  if (tag_name === img_TAG_NAME) {
+  if (tag_name === IMG_TAG_NAME) {
     const alt_text = target.getAttribute("alt");
-    alert(alt_text);
-    // alert("클릭" + tag_name + alt_text);
+    // alert("클릭 : " + tag_name + alt_text);
     if (alt_text === ALT_TEXT.인증사진) {
       document.location.href = "./01.html";
     } else if (alt_text === ALT_TEXT.바라보기) {
--- UI_005/js/nav2.js
+++ UI_005/js/nav2.js
@@ -1,3 +1,4 @@
+// DOM 객체를 변수에 할당하여 JS 코드에서 사용할 준비하기
 const nav_list = document.querySelectorAll("li.main_nav");
 // const nav_login = document.querySelector("li.main_nav.login");
 // const nav_join = document.querySelector("li.main_nav.join");
@@ -5,7 +6,7 @@ const nav_list = document.querySelectorAll("li.main_nav");
 const nav_click_handler = (event) => {
   const target = event.target;
   const nav_text = target.innerText;
-  alert(nav_text + "메뉴 클릭");
+  alert(nav_text + " 메뉴 클릭");
 };
 
 // nav_list[0].addEventListener("click", nav_click_handler);
@@ -13,19 +14,17 @@ const nav_click_handler = (event) => {
 // nav_list[2].addEventListener("click", nav_click_handler);
 
 // for (let i = 0; i < nav_list.length; i++) {
-//   nav_list[i];
-//   addEventListener("click", nav_click_handler);
+//   nav_list[i].addEventListener("click", nav_click_handler);
 // }
-// 확장된 for문
 
 // const add_event = (item) => {
 //   item.addEventListener("click", nav_click_handler);
 // };
-// nav_list.forEach(add_event) ;
+// nav_list.forEach(add_event);
 
-/*
-JS에서는 함수의 매개변수로 직접 함수를 전달할 수 있다
-*/
+/**
+ * JS 에서는 함수의 매개변수로 직접 함수를 전달할수 있다.
+ */
 nav_list.forEach((item) => {
   item.addEventListener("click", nav_click_handler);
 });
